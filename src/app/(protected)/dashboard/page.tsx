ad96e6ea29cf44f041d526920b6e4ac72b010270
Ankit-Raj27 <ankitrajk27@gmail.com>
2024-12-11 03:38:03 +0530

    [connected db and backend]

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,13 +1,15 @@
-import Sidebar from '@/components/global/sidebar'
-import React from 'react'
-
-
+import { onBoardUser } from '@/actions/user'
+import { redirect } from 'next/navigation'
 
 const page = async () => {
+  const user = await onBoardUser()
+  if (user.status === 200 || user.status === 201) {
+    return redirect(`/dashboard/${user.data?.firstname}${user.data?.lastname}`)
+  }
+
   return (
     // Server action to onBoard user
-    
-    <div>Paje</div>
+    redirect('/sign-in')
   )
 }
 
--- src/components/global/infobar/index.tsx
+++ src/components/global/infobar/index.tsx
@@ -13,7 +13,8 @@ import Upgrade from '../sidebar/upgrade'
 import CreateAutomation from '../create-automation'
 import Search from './search'
 import Notification from './notification'
-import MainBreadCrumb from '../mainbreadcrumb'
+import MainBreadCrumb from '../bread-crumbs/mainbreadcrumb'
+import { LogoSmall } from '@/svgs/logo-small'
 
 
 type Props = {
@@ -24,46 +25,61 @@ type Props = {
 const Infobar = ({ slug }: Props) => {
     const { page } = usePaths()
     const currentPage = PAGE_BREAD_CRUMBS.includes(page) || page == slug
-    return (currentPage && <div className='flex flex-col'>
-        <div className='flex gap-x-3 lg:gap-x-5 justify-end'>
-            <span className='lg:hidden flex items-center flex-1 gap-x-2'>
-                <Sheet trigger={<Menu />} className="lg:hidden" >
-                    <div className='flex flex-col gap-y-5 w-full h-full p-3
-             bg-[#0e0e0e] opacity-90 bg-clip-padding backdrop-filter
-              backdrop--blur__safari backdrop-blur-3xl'>
 
-                        <div className='flex flex-col py-3'>
-                            <Items page={page} slug={slug} />
-                        </div>
-                        <div className='px-16'>
-                            <Separator orientation='horizontal' className='bg-[#333336] border-1' />
-                        </div>
-                        <div className='px-3 flex flex-col gap-y-5'>
-                            <div className='flex gap-x-2 '>
-                                <ClerkAuthState />
-                                <p className='text-[#9B9CA0]'> Profile </p>
-                            </div>
-                            <div className='flex gap-x-3'>
-                                <HelpDuoToneWhite />
-                                <p>Help</p>
-                            </div>
-                        </div>
-                        <SubscriptionPlan type='FREE'>
-                            <div className='flex flex-1 flex-col justify-end'>
-                                <Upgrade />
-                            </div>
-                        </SubscriptionPlan>
+    return (
+        currentPage && (
+          <div className="flex flex-col">
+            <div className="flex gap-x-3 lg:gap-x-5 justify-end">
+              <span className="lg:hidden flex items-center flex-1 gap-x-2">
+                <Sheet
+                  trigger={<Menu />}
+                  className="lg:hidden"
+                >
+                  <div className="flex flex-col gap-y-5 w-full h-full p-3 bg-[#0e0e0e] bg-opacity-90 bg-clip-padding backdrop-filter backdrop--blur__safari backdrop-blur-3xl">
+                    <div className="flex gap-x-2 items-center p-5 justify-center">
+                      <LogoSmall />
                     </div>
+                    <div className="flex flex-col py-3">
+                      <Items
+                        page={page}
+                        slug={slug}
+                      />
+                    </div>
+                    <div className="px-16">
+                      <Separator
+                        orientation="horizontal"
+                        className="bg-[#333336]"
+                      />
+                    </div>
+                    <div className="px-3 flex flex-col gap-y-5">
+                      <div className="flex gap-x-2">
+                        <ClerkAuthState />
+                        <p className="text-[#9B9CA0]">Profile</p>
+                      </div>
+                      <div className="flex gap-x-3">
+                        <HelpDuoToneWhite />
+                        <p className="text-[#9B9CA0]">Help</p>
+                      </div>
+                    </div>
+                    <SubscriptionPlan type="FREE">
+                      <div className="flex-1 flex flex-col justify-end">
+                        <Upgrade />
+                      </div>
+                    </SubscriptionPlan>
+                  </div>
                 </Sheet>
-            </span>
-            <Search />
-            <CreateAutomation />
-            <Notification></Notification>
-        </div>
-        <MainBreadCrumb
-            slug={slug}
-        />
-    </div>)
+              </span>
+              <Search />
+              <CreateAutomation />
+              <Notification />
+            </div>
+            <MainBreadCrumb
+              page={page === slug ? 'Home' : page}
+              slug={slug}
+            />
+          </div>
+        )
+      )
 }
 
 export default Infobar
