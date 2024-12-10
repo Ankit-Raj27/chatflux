429e0c20c0b19ba9ae3fc19546ad454a9f3bee75
Ankit-Raj27 <ankitrajk27@gmail.com>
2024-12-11 01:34:23 +0530

    [set up the automation page]

---
+++ src/app/(protected)/dashboard/[slug]/automations/[id]/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+    // WIP : Connect real automations list
+    return (
+        <div className=" flex flex-col items-center gap-y-20">
+            
+        </div>
+    )
+}
+
+export default page
